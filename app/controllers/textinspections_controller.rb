23155bec391398ee0e16485c591c96ba027331a9
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-18 19:07:55 -0400

    Add word count, estimated reading time and most common words features

--- app/controllers/textinspections_controller.rb
+++ app/controllers/textinspections_controller.rb
@@ -2,4 +2,15 @@ class TextinspectionsController < ApplicationController
 	def new
 		render 'new'
 	end
+
+	def create
+  		@text = params[:text_inspection][:user_text]
+  		@words = @text.split(" ")
+  		@word_count = @words.length
+  		@reading_time = @word_count/275
+  		@frequency = Hash.new(0)
+  		@words.each{|key| @frequency[key] += 1}
+  		@frequency = @frequency.sort_by{|a,b| -b}.first(10)
+    	render 'results'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get '/text_inspections/new', to: 'textinspections#new'
 
+  post '/text_inspections', to: 'textinspections#create'
+
+  get '/asciis/new', to: 'asciis#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
