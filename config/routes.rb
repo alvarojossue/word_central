e316902514e0805001974e7c879a8ac8b3bb8ba9
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-18 15:17:13 -0400

    Add route, controller and view for Text Inspections

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'site#home'
 
+  get '/text_inspections/new', to: 'textinspections#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
